4d61c5500609c6873452e18128c0d6e4c61119f0
Joel Gregory <joel@vistametalsgeorgia.com>
2016-11-03 09:14:58 -0400

    Added read/write methods

--- Serial_Connection.py
+++ Serial_Connection.py
@@ -5,5 +5,9 @@ import serial
 port_name = input("Enter the name of the port\n")
 
 ser = serial.Serial(port_name)
-while(1 > 0):
-    print(ser.read())
+def readme():
+    while(1 > 0):
+        print(ser.read())
+def writeme():
+    msg = input("Input the message")
+    ser.write(int(msg))
